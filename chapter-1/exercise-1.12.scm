19ca236ca10ba19415f2e01dae9d43e7df8f2a29
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-06-13 14:53:44 +0300

    Add exercise 1.12

---
+++ chapter-1/exercise-1.12.scm
@@ -0,0 +1,36 @@
+;; Not sure if this conforms to the Scheme used in SICP.
+;; The following code is R6RS Scheme.
+
+(define (factorial n)
+  (define (fact-iter product counter max-count)
+    (if (> counter max-count)
+        product
+        (fact-iter (* counter product)
+                   (+ counter 1)
+                   max-count)))
+  (fact-iter 1 1 n))
+
+(define (combination n r)
+  (/ (factorial n)
+     (* (factorial r)
+        (factorial (- n r)))))
+
+
+(define (pascal n-rows)
+  (define (pascal-iter iter triangle)
+    (if (= iter n-rows)
+        (reverse triangle)
+        (pascal-iter (add1 iter)
+                     (cons
+                       (map (lambda (x) (combination iter x))
+                            (iota (add1 iter)))
+                       triangle))))
+  (pascal-iter 0 '()))
+
+(define (display-triangle rest-triangle)
+    (when (not (null? rest-triangle))
+      (display (car rest-triangle))
+      (newline)
+      (display-triangle (cdr rest-triangle))))
+
+(display-triangle (pascal 7))
